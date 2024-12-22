14b5ff6abb2fc30b9b2d2e468b655247f2cda9b7
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2024-12-22 13:59:19 +0100

    Add system that lets `Dog`s `bark`

---
+++ examples/005-barking_dog/main.rs
@@ -0,0 +1,22 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Dog;
+
+fn add_dogs(mut commands: Commands) {
+    commands.spawn(Dog);
+}
+
+fn bark(dogs: Query<&Dog>) {
+    for _ in dogs.iter() {
+        println!("Woof");
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_systems(Startup, add_dogs).add_systems(Update, bark);
+
+    app.run();
+}
