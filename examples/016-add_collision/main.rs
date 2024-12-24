0e6da23ed4a21b854e59f55897a502b0a6b236e7
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2024-12-24 15:15:58 +0100

    Add collision detection and reflection

---
+++ examples/016-add_collision/main.rs
@@ -0,0 +1,180 @@
+use bevy::{
+    math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume},
+    prelude::*,
+    render::camera::ScalingMode,
+};
+
+const MAX_X: f32 = 1920.0;
+const MAX_Y: f32 = 1200.0;
+const WALL_THICKNESS: f32 = 20.0;
+const BALL_RADIUS: f32 = 12.0;
+const BALL_SPEED: f32 = 600.0;
+
+#[derive(Component)]
+struct Ball {
+    velocity: Vec2,
+}
+
+#[derive(Component)]
+struct Collider;
+
+enum WallLocation {
+    Top,
+    Bottom,
+    Left,
+    Right,
+}
+
+impl WallLocation {
+    fn position(&self) -> Vec3 {
+        match self {
+            WallLocation::Top => Vec3::new(0.0, MAX_Y / 2.0, 0.0),
+            WallLocation::Bottom => Vec3::new(0.0, -MAX_Y / 2.0, 0.0),
+            WallLocation::Left => Vec3::new(-MAX_X / 2.0, 0.0, 0.0),
+            WallLocation::Right => Vec3::new(MAX_X / 2.0, 0.0, 0.0),
+        }
+    }
+
+    fn size(&self) -> Vec3 {
+        match self {
+            WallLocation::Bottom | WallLocation::Top => Vec3::new(MAX_X, WALL_THICKNESS, 0.0),
+            WallLocation::Left | WallLocation::Right => Vec3::new(WALL_THICKNESS, MAX_Y, 0.0),
+        }
+    }
+}
+
+struct SpawnWall {
+    location: WallLocation,
+}
+
+impl Command for SpawnWall {
+    fn apply(self, world: &mut World) {
+        world.spawn((
+            Sprite::from_color(Color::WHITE, Vec2::ONE),
+            Transform::from_translation(self.location.position()).with_scale(self.location.size()),
+            Collider,
+        ));
+    }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scaling_mode: ScalingMode::AutoMin {
+                min_width: MAX_X,
+                min_height: MAX_Y,
+            },
+            ..OrthographicProjection::default_2d()
+        }),
+    ));
+
+    commands.queue(SpawnWall {
+        location: WallLocation::Top,
+    });
+    commands.queue(SpawnWall {
+        location: WallLocation::Bottom,
+    });
+    commands.queue(SpawnWall {
+        location: WallLocation::Left,
+    });
+    commands.queue(SpawnWall {
+        location: WallLocation::Right,
+    });
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/ball.png")),
+        Ball {
+            velocity: Vec2::new(0.5, 0.5).normalize() * BALL_SPEED,
+        },
+    ));
+}
+
+fn apply_velocity(mut balls: Query<(&Ball, &mut Transform)>, time: Res<Time>) {
+    for (ball, mut transform) in &mut balls {
+        transform.translation.x += ball.velocity.x * time.delta_secs();
+        transform.translation.y += ball.velocity.y * time.delta_secs();
+    }
+}
+
+fn check_for_collisions(
+    mut balls: Query<(&mut Ball, &Transform)>,
+    obstacles: Query<&Transform, With<Collider>>,
+) {
+    for (mut ball, ball_transform) in &mut balls {
+        for obstacle in &obstacles {
+            let collision = ball_collision(
+                BoundingCircle::new(ball_transform.translation.truncate(), BALL_RADIUS),
+                Aabb2d::new(
+                    obstacle.translation.truncate(),
+                    obstacle.scale.truncate() / 2.,
+                ),
+            );
+
+            if let Some(collision) = collision {
+                // Reflect the ball's velocity when it collides
+                let mut reflect_x = false;
+                let mut reflect_y = false;
+
+                // Reflect only if the velocity is in the opposite direction of the collision
+                // This prevents the ball from getting stuck inside the bar
+                match collision {
+                    Collision::Left => reflect_x = ball.velocity.x > 0.0,
+                    Collision::Right => reflect_x = ball.velocity.x < 0.0,
+                    Collision::Top => reflect_y = ball.velocity.y < 0.0,
+                    Collision::Bottom => reflect_y = ball.velocity.y > 0.0,
+                }
+
+                // Reflect velocity on the x-axis if we hit something on the x-axis
+                if reflect_x {
+                    ball.velocity.x = -ball.velocity.x;
+                }
+
+                // Reflect velocity on the y-axis if we hit something on the y-axis
+                if reflect_y {
+                    ball.velocity.y = -ball.velocity.y;
+                }
+            }
+        }
+    }
+}
+
+enum Collision {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+// Returns `Some` if `ball` collides with `bounding_box`.
+// The returned `Collision` is the side of `bounding_box` that `ball` hit.
+fn ball_collision(ball: BoundingCircle, bounding_box: Aabb2d) -> Option<Collision> {
+    if !ball.intersects(&bounding_box) {
+        return None;
+    }
+
+    let closest = bounding_box.closest_point(ball.center());
+    let offset = ball.center() - closest;
+    let side = if offset.x.abs() > offset.y.abs() {
+        if offset.x < 0. {
+            Collision::Left
+        } else {
+            Collision::Right
+        }
+    } else if offset.y > 0. {
+        Collision::Top
+    } else {
+        Collision::Bottom
+    };
+
+    Some(side)
+}
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (apply_velocity, check_for_collisions))
+        .run();
+}
